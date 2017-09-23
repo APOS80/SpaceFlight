9bb8447b485296fedaa108a97aacc251f34720d3
Andreas Olsson <photoguy.apo@gmail.com>
2017-09-24 00:41:53 +0200

    Better flying! But you cant do a loop yet, getting there.

--- SpaceFlight.rkt
+++ SpaceFlight.rkt
@@ -5,50 +5,18 @@
          math/flonum)
  
 
-;values
+;Structs
+(struct item (type position direction roll speed) #:mutable #:transparent) 
+
+;Mutables
 (define mouse-x 0)
 (define mouse-y 0)
+(define key-ad 0)
+(define key-ws 0)
 
-(define cam-angle 0) ;Roll
-(define cam-dir (dir 0 0 1)) ;Direction
-(define cam-pos (pos 1000 1000 994)) ;position
-(define cam-speed 0) ;cam speed
+(define camOne (item 'camera (pos 1000 1000 994) (dir 0 0 1) 0 0))
 
 ;Helpers
-(define (on-mouse  s n t x y e) ;Get directions
-  (let ([mx (- x 250)]
-        [my (- y 250)])
-    (if (or (> mx -250) (< mx 250) (> my -250)(< my 250))
-        (if (and (< (abs mx) 10) (< (abs my) 10))
-            (begin
-         (set! mouse-x 0)
-         (set! mouse-y 0))
-        (begin
-         (set! mouse-x mx)
-         (set! mouse-y my)))
-        
-        (begin
-         (set! mouse-x 0)
-         (set! mouse-y 0))
-        )
-    ))
-
-(define (on-key s n t k) ;Respond to key events
-  (begin
-  (case k
-    [("a") (set! cam-angle (- cam-angle 0.5))]
-    [("d") (set! cam-angle (+ cam-angle 0.5))]
-    [("w") (set! cam-speed (+ cam-speed 0.1))]
-    [("s") (set! cam-speed (- cam-speed 0.1))]
-    )
-  (printf "cam-speed: ~a\n" cam-speed)
-  ))
-
-(define (corr-angles a)
-  (cond
-    [(> a 360) (- a 360)]
-    [(< a 0) (- 360 a)]
-    [else a]))
 
 (define (angles->dir-yp ang1 ang2 axis) 
   (let ([ang1  (fl (degrees->radians ang1))]
@@ -72,6 +40,51 @@
       )
     ))
 
+(define (dir->angles-yp dv axis)
+    (let* ([x (fl(dir-dx dv))]
+           [y (fl(dir-dy dv))]
+           [z (fl(dir-dz dv))]
+           [r (flsqrt (fl+ (fl* x x) (fl+ (fl* y y) (fl* z z))))])
+      (cond
+        ;Yaw around Z axis
+        [(equal? 'zx axis) (list (radians->degrees (atan y x))
+                            (radians->degrees (asin (fl/ z r))))] ;Yaw from x, Pitch counterclockwise.
+        [(equal? 'zy axis) (list (radians->degrees (atan x y))
+                            (radians->degrees (asin (fl/ z r))))] ;Yaw from y, Pitch counterclockwise.
+        ;Yaw around y axis
+        [(equal? 'yz axis) (list (radians->degrees (atan x z))
+                            (radians->degrees (asin (fl/ y r))))] ;Yaw from z, Pitch counterclockwise.
+        [(equal? 'yx axis) (list (radians->degrees (atan z x))
+                            (radians->degrees (asin (fl/ y r))))] ;Yaw from x, Pitch counterclockwise.
+        ;Yaw around x axis
+        [(equal? 'xy axis) (list (radians->degrees (atan z y))
+                            (radians->degrees (asin (fl/ x r))))] ;Yaw from y, Pitch counterclockwise.
+        [(equal? 'xz axis) (list (radians->degrees (atan y z))
+                            (radians->degrees (asin (fl/ x r))))] ;Yaw from z, Pitch counterclockwise.
+        [else (list 0.0 0.0)]
+         )
+  ))
+
+(define (recalc-cam cam mx my ad ws)
+  (let*
+      ([di (dir-normalize (dir+ (item-direction cam)
+                                (transform-dir
+                                 (angles->dir-yp ad (/ (* -1 my) 20) 'yz)
+                                 
+                                  (point-at
+                                   (item-position camOne)
+                                   (item-direction camOne)
+                                   #:angle (item-roll camOne)
+                                   #:up -y)
+                                 )))]
+       [po (pos+ (item-position cam) (dir-scale di ws))]
+       [ro (+ (item-roll cam) (/ mx 20))])
+    (item 'camera
+          po
+          di
+          ro
+          ws)))
+
 ;stuff
 (current-material (material #:ambient 0.01
                             #:diffuse 0.39
@@ -89,7 +102,7 @@
              (sphere (pos 1000 1000 1000) 1)))
            (with-color (rgba "red")
              (with-emitted (emitted "red" 5)
-             (sphere (pos 2000 2000 2000) 1)))
+             (sphere (pos 1020 1020 1020) 1)))
    
            ))
 
@@ -114,31 +127,47 @@
              (with-emitted (emitted "blue" 2)
              (arrow (pos 1000 1000 1000) (pos 1000 1000 1010))))
            ))
-           
 
-;Draw stuff
+;BigBang stuff
+(define (on-mouse  s n t x y e) ;Get directions
+  (let ([mx (- x 250)]
+        [my (- y 250)])
+    (if (or (> mx -250) (< mx 250) (> my -250)(< my 250))
+        (if (and (< (abs mx) 10) (< (abs my) 10))
+            (begin
+         (set! mouse-x 0)
+         (set! mouse-y 0))
+        (begin
+         (set! mouse-x mx)
+         (set! mouse-y my)))
+        
+        (begin
+         (set! mouse-x 0)
+         (set! mouse-y 0))
+        )
+    ))
+
+(define (on-key s n t k) ;Respond to key events
+  (begin
+  (case k
+    [("a") (set! key-ad (- key-ad 1))]
+    [("d") (set! key-ad (+ key-ad 1))]
+    [("w") (set! key-ws (+ key-ws 0.1))]
+    [("s") (set! key-ws (- key-ws 0.1))]
+    )
+  ))
+
+
 (define (on-draw s n t)
   (begin
+    (set! camOne (recalc-cam camOne mouse-x mouse-y key-ad key-ws))
     
-    (set! cam-angle (corr-angles cam-angle))
-    (set! cam-dir (dir-normalize (dir+ cam-dir
-                                       (dir-scale
-                                       (transform-dir
-                                        (dir-normalize
-                                         (dir mouse-x mouse-y 1))
-                                        (point-at cam-pos cam-dir #:angle cam-angle #:up -y ))
-                                       0.01)
-                                       
-                                       )))
-    
-
-    (set! cam-pos (pos+ cam-pos (dir-scale cam-dir cam-speed)))
-
     (combine lights
              orientation
              objects
-             (basis 'camera (point-at cam-pos cam-dir #:angle cam-angle #:up -y ))
-             )          
+             (basis 'camera (point-at (item-position camOne)
+                                      (item-direction camOne)
+                                      #:angle (item-roll camOne) #:up -y )))          
   ))
  
 (big-bang3d 0  #:on-mouse on-mouse #:on-key on-key #:on-draw on-draw)
